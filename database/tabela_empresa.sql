6ab03ced6016b9ab91765310c08211253a86eeb8
Pedro Henrique Reis Lima <pedro23reislima@gmail.com>
2024-10-31 00:40:16 -0300

    feat: Trocas das declarações das colunas cnpj e cpf. Cometi um erro na hora de declarar, usei BIT, onde poderia ter simplismente usado char, vi isso na hora de inserir registros.

--- database/tabela_empresa.sql
+++ database/tabela_empresa.sql
@@ -2,7 +2,7 @@ create table empresas (
 	id int not null,
     nome varchar(100) not null,
     razao_social varchar(150) not null,
-    cnpj bit(14) not null unique,
+    cnpj char(14) not null unique,
     endereco varchar(200),
     tipo varchar(8) not null,
     primary key (id)
